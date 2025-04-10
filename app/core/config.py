d1edb2294521d4534db7e46082f53c1a2ccf12ec
saisoftsuave <sai.babu@softsuave.org>
2025-04-10 13:01:15 +0530

    worked on settings(env values)

---
+++ app/core/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    HOST: str
+    DATABASE_URL: str
+    DATABASE_URL_DOCKER: str
+    TEST_DATABASE_URL: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+    INVITE_TOKEN_EXPIRE_TIME: int
+    JWT_SECRET_KEY: str
+    JWT_REFRESH_SECRET_KEY: str
+    ALGORITHM: str
+    SMTP_SERVER: str
+    SMTP_PORT: int
+    EMAIL_ADDRESS: str
+    EMAIL_PASSWORD: str
+    AWS_ACCESS_KEY: str
+    AWS_SECRET_KEY: str
+    AWS_REGION: str
+    S3_BUCKET_NAME: str
+    S3_FOLDER_NAME: str
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+Config = Settings()
