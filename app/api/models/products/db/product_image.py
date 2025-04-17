9515ba89167413c5eef444167dbe80a1a9426cf6
saisoftsuave <sai.babu@softsuave.org>
2025-04-17 15:46:47 +0530

    worked on product image routes

--- app/api/models/products/db/product_image.py
+++ app/api/models/products/db/product_image.py
@@ -6,7 +6,7 @@ from sqlmodel import SQLModel, Field, Relationship
 
 class ProductImage(SQLModel, table=True):
     __tablename__ = "product_images"
-    id: uuid.UUID = Field(primary_key=True, default=uuid.uuid4())
+    id: uuid.UUID = Field(primary_key=True, default_factory=uuid.uuid4)
     product_id: uuid.UUID = Field(foreign_key="products.id")
     image: str
     primary_image: bool = Field(default=False)
